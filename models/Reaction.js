51121adedf15d61adb11361f66d1c311ad8e27be
lalitakapadia <lalita.kapadia@gmail.com>
2024-02-10 14:30:00 +0000

    start working thoughtRoutes and doing test on insomnia

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,30 +2,33 @@ const {Schema, Types} = require('mongoose');
 const dateFormat = require("../utils/dateFormat");
 
 
-const reactionSchema = new Schema({
-    reactionID: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+           //get: timestamp => dateFormat(timestamp)
+        }
     },
-    reactionBody: {
-        type: String,
-        required: true,
-        maxLength: 280
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: timestamp => dateFormat(timestamp)
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
     }
-}, {
-    toJSON: {
-        getters: true
-    },
-    id: false
-});
+);
 
 module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -26,17 +26,25 @@ const userSchema = new Schema({
             ref: "User"
         }
     ]
-}, {
-    toJSON: {
-        virtuals: true
-    },
-    id: false
+},
+    {
+        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+        // Here we are indicating that we want virtuals to be included with our reaction, overriding the default behavior
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// Create a virtual property `friendCount` that gets the user's friends
+userSchema
+.virtual("friendCount")
+.get(function () {
+    return this.friends.length;
 });
 
-userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
-})
-
+// Initialize our User model
 const User = model("User", userSchema);
 
 module.exports = User;
